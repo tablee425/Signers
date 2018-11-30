58ff6b45c23def7c94e254d057d6d5688d18a4b6
Emzari Chabashvili <emzo.emzo.chabo.1@gmail.com>
2018-11-30 23:49:47 +0800

    fetch admin list

--- src/pages/DefaultPages/ClientPage/Client/index.js
+++ src/pages/DefaultPages/ClientPage/Client/index.js
@@ -91,7 +91,21 @@ class Client extends React.Component {
       .post(`${baseUrl}/admin/list`, {})
       .then(res => {
         if (res.data.success) {
-          alert(res.data.total)
+          let tableData = [];
+          res.data.data.map((item, index) => {
+            tableData.push(
+              {
+                "key": `${index}`,
+                "Picture": "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png",
+                "name": item.firstName,
+                "tp": 18,
+                "ap": 7,
+                "os": 7,
+                "position": "5516 Adolfo Green"
+              }
+            )
+          })
+          this.setState({ tableData })
         } else {
         }
       })
