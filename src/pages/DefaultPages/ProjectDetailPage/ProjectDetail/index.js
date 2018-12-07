ce885061926d67fc6c66b2f95271ec0fa465f073
Emzari Chabashvili <emzo.emzo.chabo.1@gmail.com>
2018-12-07 22:18:36 +0800

    cs

--- src/pages/DefaultPages/ProjectDetailPage/ProjectDetail/index.js
+++ src/pages/DefaultPages/ProjectDetailPage/ProjectDetail/index.js
@@ -1,8 +1,5 @@
 import React from 'react'
-import {
-  Form,
-  Button,
-} from 'antd'
+import { Form, Button } from 'antd'
 import './style.scss'
 import { Redirect } from 'react-router'
 import axios from 'axios'
@@ -20,13 +17,13 @@ class ProjectDetail extends React.Component {
   }
 
   componentDidMount() {
-    this.getProjectInfo();
+    this.getProjectInfo()
   }
 
   getProjectInfo = () => {
     axios
       .post(`${baseUrl}/project/admin`, {
-        project_id: config.projectKey
+        project_id: config.projectKey,
       })
       .then(res1 => {
         if (res1.data.success) {
@@ -67,15 +64,17 @@ class ProjectDetail extends React.Component {
     let projectType = project ? project.type : ''
     let projectCost = project ? `$${project.cost}` : ''
     let votersNeeded = project ? project.expected_voters : ''
-    let volunteersFlag = project ? project.expects_volunteers ? 'Yes' : 'No' : ''
-    let donationsFlag = project ? project.expects_donations ? 'Yes' : 'No' : ''
+    let volunteersFlag = project ? (project.expects_volunteers ? 'Yes' : 'No') : ''
+    let donationsFlag = project ? (project.expects_donations ? 'Yes' : 'No') : ''
     let donationsValue = project ? `$${project.donations_value}` : ''
     let donationsUrl = project ? project.donations_url : ''
-    let pricePerVoters = project ? `${project.unit_voters} voters = $${project.price_for_signers_for_100_voters}` : ''
+    let pricePerVoters = project
+      ? `${project.unit_voters} voters = $${project.price_for_signers_for_100_voters}`
+      : ''
     let location = project ? `${project.country} - ${project.location}` : ''
     let startEndDate = project ? `${project.date_registered} - ${project.date_expiration}` : ''
     let description = project ? project.description : ''
-    
+
     return (
       <div>
         <div className="row">
@@ -92,9 +91,7 @@ class ProjectDetail extends React.Component {
                   marginTop: 15,
                 }}
               >
-                <h1 style={{ fontWeight: 'bold' }}>
-                  { projectName }
-                </h1>
+                <h1 style={{ fontWeight: 'bold' }}>{projectName}</h1>
                 <div className="row" style={{ marginRight: 30, marginTop: 2 }}>
                   <Button
                     style={{ width: 140, height: 35, marginRight: 15, backgroundColor: '#D1D8E0' }}
@@ -114,56 +111,58 @@ class ProjectDetail extends React.Component {
             <div className="row">
               <div style={{ marginTop: 15 }}>
                 <label className="productDetailPage__blockLabel">Project Type</label>
-                <label className="productDetailPage__marginTopLabel">{ projectType }</label>
+                <label className="productDetailPage__marginTopLabel">{projectType}</label>
               </div>
               <div style={{ marginTop: 15, marginLeft: 25 }}>
                 <label className="productDetailPage__blockLabel">Project Cost</label>
-                <label className="productDetailPage__marginTopLabel">{ projectCost }</label>
+                <label className="productDetailPage__marginTopLabel">{projectCost}</label>
               </div>
               <div style={{ marginTop: 15, marginLeft: 25 }}>
                 <label className="productDetailPage__blockLabel">Voters Needed</label>
-                <label className="productDetailPage__marginTopLabel">{ votersNeeded }</label>
+                <label className="productDetailPage__marginTopLabel">{votersNeeded}</label>
               </div>
               <div style={{ marginTop: 15, marginLeft: 25 }}>
                 <label className="productDetailPage__blockLabel">Accepts Volunteers</label>
-                <label className="productDetailPage__marginTopLabel">{ volunteersFlag }</label>
+                <label className="productDetailPage__marginTopLabel">{volunteersFlag}</label>
               </div>
               <div style={{ marginTop: 15, marginLeft: 25 }}>
                 <label className="productDetailPage__blockLabel">Accepts Donations</label>
-                <label className="productDetailPage__marginTopLabel">{ donationsFlag }</label>
+                <label className="productDetailPage__marginTopLabel">{donationsFlag}</label>
               </div>
             </div>
 
             <div className="row" style={{ marginTop: 20 }}>
               <div style={{ marginTop: 15 }}>
                 <label className="productDetailPage__blockLabel">Donations Value</label>
-                <label className="productDetailPage__marginTopLabel">{ donationsValue }</label>
+                <label className="productDetailPage__marginTopLabel">{donationsValue}</label>
               </div>
               <div style={{ marginTop: 15, marginLeft: 25 }}>
                 <label className="productDetailPage__blockLabel">Donations Url</label>
-                <label className="productDetailPage__marginTopLabel" style={{ color: '#378EFF' }}>{ donationsUrl }</label>
+                <label className="productDetailPage__marginTopLabel" style={{ color: '#378EFF' }}>
+                  {donationsUrl}
+                </label>
               </div>
               <div style={{ marginTop: 15, marginLeft: 50 }}>
                 <label className="productDetailPage__blockLabel">Price Per Voters</label>
-                <label className="productDetailPage__marginTopLabel">{ pricePerVoters }</label>
+                <label className="productDetailPage__marginTopLabel">{pricePerVoters}</label>
               </div>
             </div>
 
             <div className="row" style={{ marginTop: 20 }}>
               <div style={{ marginTop: 15 }}>
                 <label className="productDetailPage__blockLabel">Location</label>
-                <label className="productDetailPage__marginTopLabel">{ location }</label>
+                <label className="productDetailPage__marginTopLabel">{location}</label>
               </div>
               <div style={{ marginTop: 15, marginLeft: 25 }}>
                 <label className="productDetailPage__blockLabel">Start / End Date</label>
-                <label className="productDetailPage__marginTopLabel">{ startEndDate }</label>
+                <label className="productDetailPage__marginTopLabel">{startEndDate}</label>
               </div>
             </div>
 
             <div className="row" style={{ marginTop: 30 }}>
               <div style={{ marginTop: 15 }}>
                 <label className="productDetailPage__blockLabel">Description</label>
-                <label className="productDetailPage__descLabel">{ description }</label>                  
+                <label className="productDetailPage__descLabel">{description}</label>
               </div>
             </div>
           </div>
